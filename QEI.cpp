e79d52b4155fca3b613abb4b84731331b8a84d13
Benoit Rapidel <benoit.rapidel+devs@exmachina.fr>
2017-01-23 13:20:24 +0100

    Ensure correct initialization mode

--- QEI.cpp
+++ QEI.cpp
@@ -12,9 +12,13 @@ QEI::QEI(QEIConfig_t opts) {
 
     if (opts & QEI_SIGNAL_MODE)
         qei_set_signal_mode(true);      // Set encoder mode to DIR/STEP mode (default A & B)
+    else
+        qei_set_signal_mode(false);      // Set encoder mode to DIR/STEP mode (default A & B)
 
     if (opts & QEI_4XCOUNT)
         qei_set_capture_mode(true);     // Count A & B edges (more resolution but less range)
+    else
+        qei_set_capture_mode(false);     // Count A & B edges (more resolution but less range)
 
     qei_set_max_pulses(0xFFFFFFFF);     // Allow QEI to count up to maximum range
     qei_set_filter(3);                  // Digital filter (debounce). In order
